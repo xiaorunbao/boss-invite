8cf083308e098418781ae044de5b02e32116baca
xiaorunbao <542929781@qq.com>
2021-05-11 14:26:06 +0800

    feat: add dynamic personal page

--- src/containers/personal/personal.jsx
+++ src/containers/personal/personal.jsx
@@ -1,8 +1,61 @@
 /*用户个人中心路由组件 */
-import React, { Component } from "react";
+import { Button, List, Result, WhiteSpace, Modal } from "antd-mobile";
+import Cookies from "js-cookie";
+import React from "react";
+import { connect } from "react-redux";
+import { resetUser } from "../../redux/actions";
 
-export default class Personal extends Component {
+const Item = List.Item;
+const Brief = Item.Brief;
+
+class Personal extends React.Component {
+  handleLogout = () => {
+    Modal.alert("退出", "确认退出登录吗?", [
+      { text: "取消", onPress: () => console.log("cancel") },
+      {
+        text: "确认",
+        onPress: () => {
+          // 清除 cookie 中的 userid
+          Cookies.remove("userid");
+          // 重置 redux 中的 user 状态
+          this.props.resetUser();
+        },
+      },
+    ]);
+  };
+
   render() {
-    return <div>个人中心</div>;
+    const { username, header, post, info, salary, company } = this.props.user;
+
+    return (
+      <div style={{ marginTop: 50 }}>
+        <Result
+          img={
+            <img
+              src={require(`../../assets/imgs/${header}.png`).default}
+              style={{ width: 50 }}
+              alt="header"
+            />
+          }
+          title={username}
+          message={company}
+        />
+        <List renderHeader={() => "相关信息"}>
+          <Item multipleLine>
+            <Brief>职位: {post}</Brief>
+            <Brief>简介: {info}</Brief> <Brief>薪资: {salary}</Brief>
+          </Item>
+        </List>
+        <WhiteSpace />
+        <List>
+          <Button onClick={this.handleLogout} type="warning">
+            退出登录
+          </Button>
+        </List>
+      </div>
+    );
   }
 }
+export default connect((state) => ({ user: state.user }), { resetUser })(
+  Personal
+);
